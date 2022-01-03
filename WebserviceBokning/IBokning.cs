d69001416500b90fd63b47309242c57203f6a01f
Niklas Dahlgren <Niklas.Dahlgren640@gmail.com>
2022-01-03 12:52:02 +0100

    ffsa
    fsa

--- WebserviceBokning/IBokning.cs
+++ WebserviceBokning/IBokning.cs
@@ -8,11 +8,11 @@ using System.Text;
 
 namespace WebserviceBokning
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+   
     [ServiceContract]
     public interface IBokning
     {
 
-     
+     //HALLO
     }
 }
